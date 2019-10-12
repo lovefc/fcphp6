84b07f4679c6fdef86cf657b4ef6d1808913e70e
lovefc <fcphp@qq.com>
2019-10-12 16:06:47 +0800

    添加了db操作类，修改了路由配置

---
+++ Main/Test/db.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Main\Test;
+
+/*
+ * db 操作类
+ * @Author: lovefc 
+ * @Date: 2019-10-12 14:39:29
+ * @Last Modified by: lovefc
+ * @Last Modified time: 2019-10-12 15:48:28
+ */
+
+class db
+{
+    use \FC\Traits\Parts;
+
+    // 打印信息
+    public function index()
+    {
+        $size = $this->SQLITE->getDBSize();
+        echo 'Sqlite版本：' . $this->SQLITE->verSion() . ' 大小：' . $size[0] . $size[1] . FC_EOL;
+        
+        $size = $this->MYSQL->getDBSize();
+        echo 'Mysql版本：' . $this->MYSQL->verSion() . ' 大小：' . $size[0] . $size[1] . FC_EOL;
+    }
+
+    // 获取表的一个值
+    public function get($table = 'bizhi', $limit = 1)
+    {
+        $re = $this->SQLITE->table($table)->limit(1)->fetch();
+        \FC\pre($re);
+    }
+
+    // 获取数据库中的所有表
+    public function gettable()
+    {
+        $re = $this->SQLITE->getAllTable();
+        \FC\pre($re);
+
+        $re = $this->MYSQL->getAllTable();
+        \FC\pre($re);
+    }
+
+    // 获取表中的所有字段
+    public function getfield($table = 'ceshi')
+    {
+        $re = $this->SQLITE->table($table)->getAllField();
+        \FC\pre($re);
+        
+        $re = $this->MYSQL->table($table)->getAllField();
+        \FC\pre($re);
+    }
+}
