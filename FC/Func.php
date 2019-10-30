05d663c95401533d667b6c1f798e257ffad8572f
lovefc <fcphp@qq.com>
2019-10-30 15:38:48 +0800

    更新了一下返回值

--- FC/Func.php
+++ FC/Func.php
@@ -140,7 +140,8 @@ function setOrigin($allow_origin = false, $method = 'GET', $credentials = false)
         header('Access-Control-Allow-Methods:' . $method);
         header('Access-Control-Allow-Headers:x-requested-with,content-type');
     } else {
-        die('No access allowed'); //不允许访问
+        head(400);
+        die(); //不允许访问
     }
 }
 
