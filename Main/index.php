c977595eb77adef0fff252b7f24f33a2c41a842c
lovefc <fcphp@qq.com>
2019-11-12 14:54:29 +0800

    更新index.php文件

--- Main/index.php
+++ Main/index.php
@@ -3,8 +3,11 @@
 // 引入框架
 require dirname(__DIR__) . '/FC/Main.php';
 
-// 关闭报错
+// 报错显示
 define('DEBUG', true);
+
+// 定义错误日志
+define('LOG_DIR',__DIR__.'/Log');
 
 try {
     $obj = FC\Obj('FC\Glue\Route');
@@ -13,6 +16,6 @@ try {
     \FC\Log::Show($e->getMessage());
 }
 
-echo FC_EOL;
+//echo FC_EOL;
 
-echo round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5);
+//echo round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5);
