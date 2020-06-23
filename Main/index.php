15046a641dfeb2567dfa7e811b0faa3adf92bf49
lovefc <fcphp@qq.com>
2020-06-23 15:55:06 +0800

    顺序错误

--- Main/index.php
+++ Main/index.php
@@ -1,10 +1,10 @@
 <?php
 
-// 引入框架
-require dirname(__DIR__) . '/FC/Main.php';
-
 // 报错显示
 define('DEBUG', true);
+
+// 引入框架
+require dirname(__DIR__) . '/FC/Main.php';
 
 // 定义错误日志
 define('LOG_DIR',__DIR__.'/Log');
